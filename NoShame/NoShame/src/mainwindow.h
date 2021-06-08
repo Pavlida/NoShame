eb4c0553cac965bd281f1a82ddacf3e27ebe48c2
Pavlida <76167134+Pavlida@users.noreply.github.com>
2021-06-08 18:11:05 +0300

    Add files via upload

---
+++ NoShame/NoShame/src/mainwindow.h
@@ -0,0 +1,72 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtWidgets>
+#include "renderarea.h"
+#include "menumodel.h"
+#include "tableview.h"
+#include "categoryfilterproxymodel.h"
+#include "aboutwindow.h"
+#include "nutriens.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:    
+    Ui::MainWindow *ui;
+    RenderArea *logo;
+    MenuModel *McMenu;
+    CategoryFilterProxyModel *userMenu;
+
+    TableView *rcTable;
+    void connectTableSignals() const;
+
+    QMessageBox* cantOpen;
+    QMessageBox* sizeError;
+    QMessageBox* headerError;
+    QMessageBox* dataError;
+
+    QMessageBox* mealLimitReached;
+
+    QMap<QMessageBox*, bool> showError;
+
+    const int mealLimit = 11; //based on fullscreen data
+    QList<QString> inMeal; //name
+
+    void setUpError(QMessageBox* error);
+    void executeError(QMessageBox* error, const QString& message);
+
+    void setUpSearchShortcut();
+
+    QString properTime(int min);
+
+    AboutWindow *about;
+    Nutriens *nutriens;
+private slots:
+    void on_actionLoad_another_menu_triggered();
+    void on_actionAbout_triggered();
+    void on_actionChange_Sorting_triggered();
+    void on_SearchButton_clicked();
+    void on_actionUndo_all_changes_triggered();
+
+    void addMealItem(QModelIndex i);
+    void removeMenuItem(QModelIndex i);
+    void viewItemNutriens(QModelIndex i) const;
+
+    void on_toolButton_clicked();
+
+signals:
+    void itemAlreadyInMenu(QString);
+};
+#endif // MAINWINDOW_H
+
---
+++ NoShame/NoShame/src/tableview.cpp
@@ -0,0 +1,159 @@
+#include "tableview.h"
+#include <QHeaderView>
+#include <QLayout>
+#include <QTableView>
+#include <QWidget>
+#include <QCheckBox>
+#include <QWidgetAction>
+#include <QLabel>
+
+TableView::TableView(QWidget *parent, CategoryFilterProxyModel* m) : QWidget(parent)
+{
+    model = m;
+    QVBoxLayout *l = new QVBoxLayout(this);
+    table = new QTableView(this);
+
+    table->setModel(model);
+    table->setFont(QFont("Calibri", 9));
+
+    setupCustomMenu();
+    connect(model, SIGNAL(categoriesChanged()), SLOT(setupCategoriesMenu()));
+
+    table->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(table, SIGNAL(customContextMenuRequested(QPoint)), this,  SLOT(customMenuRequested(QPoint)));
+
+    table->horizontalHeader()->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(table->horizontalHeader(), SIGNAL(sectionClicked(int)), this, SLOT(customHeaderMenuRequested(int)));
+
+    connect(model, SIGNAL(categoryNoLongerNeeded(const QVariant&)), this, SLOT(eliminateCategory(const QVariant&)));
+
+    connect(table->horizontalHeader(), SIGNAL(sortIndicatorChanged(int, Qt::SortOrder)), this, SLOT(handleIndicatorChanged(int))); //basically for design purposes
+
+    l->addWidget(table);
+}
+
+void TableView::setupCustomMenu()
+{
+    tMenu = new QMenu(this);
+    tMenu->addAction("Add item to your meal", this, SLOT(customMenuAdd()));
+    tMenu->addAction("Remove item from the menu", this, SLOT(customMenuRemove()));
+    tMenu->addAction("View item details", this, SLOT(customMenuDetails()));
+
+}
+
+void TableView::setupCategoriesMenu()
+{
+    if(categoriesSetUp)
+        delete hMenu; // not to leak data
+    hMenu = new QMenu(this);
+    for (QVariant &category : model->getCategories())
+    {
+        QWidget *cWidget = new QWidget(hMenu); // create labels with checkable boxes
+
+        QHBoxLayout *cL = new QHBoxLayout(cWidget);
+
+        QLabel *catName = new QLabel(category.toString(), cWidget);
+        QCheckBox *checkBox = new QCheckBox(cWidget);
+        checkBox->setChecked(true);
+
+        cL->addWidget(catName);
+        cL->addWidget(checkBox, Qt::AlignRight);
+
+        QWidgetAction *cat = new QWidgetAction(hMenu);
+        cat->setDefaultWidget(cWidget);
+
+        cWidget->setObjectName(category.toString()); //for navigation
+        cat->setObjectName(category.toString() + "Action");
+
+        hMenu->addAction(cat);
+
+        connect(checkBox, &QCheckBox::stateChanged, model, [catName, this](int checked)
+        {
+            if(checked & Qt::Checked)
+            {
+                model->addCategory(catName->text());
+            }
+            else
+            {
+                model->removeCategory(catName->text());
+            }
+            model->addMode(SortFilterMode::CategoriesFilter);
+        });
+    }
+    categoriesSetUp = true;
+}
+
+void TableView::eliminateCategory(const QVariant& catName)
+{
+    QWidget *cWidget = hMenu->findChild<QWidget *>(catName.toString()); //widget and widgetaction have a weir connection so we have to find and delte them separately
+    QWidgetAction *cat = hMenu->findChild<QWidgetAction *>(catName.toString() + "Action");
+    delete cWidget;
+    delete cat;
+}
+
+void TableView::setSortingEnabled(bool v)
+{
+    table->setSortingEnabled(v);
+}
+
+void TableView::setSortabilityForModel(bool v) // Making it possible to change the sortability of the model without it being emidiately sorted after status has been changed
+{
+    isModelSortable = v;
+    if(!v)
+        setSortingEnabled(false);
+}
+
+void TableView::customMenuRequested(const QPoint& pos)
+{
+    if(!model->isEmpty())
+    {
+        rightClicked = table->indexAt(pos);
+        table->selectRow(rightClicked.row());
+        tMenu->popup(table->viewport()->mapToGlobal(pos));
+    }
+}
+
+void TableView::customMenuAdd()
+{
+    emit addItemToMeal(rightClicked);
+}
+
+void TableView::customMenuRemove()
+{
+    emit removeItemFromModel(rightClicked);
+}
+
+void TableView::customMenuDetails() const
+{
+    emit giveDetails(rightClicked);
+}
+
+
+void TableView::customHeaderMenuRequested(int logicalIndex)
+{
+    table->clearSelection();
+    if (model->headerData(logicalIndex, Qt::Horizontal) == "Category") //Not to trigger sorting after the first startup
+    {
+        QPoint p(table->horizontalHeader()->sectionPosition(logicalIndex), 0);
+        hMenu->popup(table->horizontalHeader()->viewport()->mapToGlobal(p));
+    }
+    else if (isModelSortable  && !table->isSortingEnabled())
+    {
+        setSortingEnabled(true);
+    } 
+}
+
+void TableView::handleIndicatorChanged(int logicalIndex)
+{
+    if (logicalIndex == 0)
+    {
+        setSortingEnabled(false);
+    }
+}
+
+TableView::~TableView()
+{
+    delete tMenu;
+    delete hMenu;
+    delete table;
+}
